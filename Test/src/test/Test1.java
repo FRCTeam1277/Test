250ef6ed5fb4a6ea52d1b485b88388a69f0ba5f5
MaxwellBattles <max4battles@gmail.com>
2018-02-03 15:47:51 -0500

    3rd commit

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -6,7 +6,7 @@ public class Test1
 	public static void main(String[] args)
 	{
 		System.out.println("He's a special one, Mr. Will.");
-		System.out.println("Max is better at Pinball than Will.");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
 	}
 
 }
