add19d7e836c0bc1075ad241c8cfea042eb29d11
whughson <will@hughson.ws>
2018-02-03 15:41:58 -0500

    My changes

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -5,7 +5,7 @@ public class Test1
 
 	public static void main(String[] args)
 	{
-		System.out.println("He's a special one, Mr. Will.");
+		System.out.println("He's a special one, Mr. Wall.");
 	}
 
 }
