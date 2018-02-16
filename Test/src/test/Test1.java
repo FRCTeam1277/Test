fe6a8194a8674f889bf329694b1ce1e498d4d1a4
whughson <will@hughson.ws>
2018-02-16 15:00:48 -0500

    GTGJJOKHHGOOOOOOO&&*N*(N )N)N&M^YGMHHHmN

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -17,6 +17,7 @@ public class Test1
 		System.out.println("goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, "
 				+ "goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, "
 				+ "goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon, goon");
+		will = "GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON";
 	}
 
 }
