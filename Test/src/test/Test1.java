2862b7f05a75d37bb6b58bc583a77e8f26278df4
whughson <will@hughson.ws>
2018-02-16 14:14:54 -0500

    test me

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -5,12 +5,8 @@ public class Test1
 
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 		System.out.println("He's a special one, Mr. Wall.");
-=======
-		System.out.println("He's a special one, Mr. Will.");
-		System.out.println("Max is better at Pinball than Will.");
->>>>>>> cc586aad94029829d1f7ec018ebd62cb4fbf307e
+		System.out.println("I want be written");
 	}
 
 }
