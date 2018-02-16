a23c94cbe0ecbf4f517f1b1779b9357b917b1e0c
MaxwellBattles <max4battles@gmail.com>
2018-02-16 14:44:53 -0500

    Will's commit

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -6,17 +6,7 @@ public class Test1
 	public static void main(String[] args)
 	{
 		System.out.println("He's a special one, Mr. Wall.");
-<<<<<<< HEAD
-
-		System.out.println("He's a special one, Mr. Will.");
-
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
-
-		System.out.println("Max is better at Pinball than Will.");
-
-=======
 		System.out.println("I want be written");
->>>>>>> 2862b7f05a75d37bb6b58bc583a77e8f26278df4
 	}
 
 }
