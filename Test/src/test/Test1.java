738f3a58f10b131f7c3a4d54cd5e79bc809e0b0f
MaxwellBattles <max4battles@gmail.com>
2018-02-16 14:51:35 -0500

    Will is a goooooooooon

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -6,6 +6,13 @@ public class Test1
 	public static void main(String[] args)
 	{
 		System.out.println("He's a special one, Mr. Wall.");
+		String will = "goon";
+		if(will.equals("goon"))
+		{
+			System.out.println("Will is a goon.");
+		}
+		else
+			System.out.println("Will is still a goon.");
 		System.out.println("I want be written");
 	}
 
